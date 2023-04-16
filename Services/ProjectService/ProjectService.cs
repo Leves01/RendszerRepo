18b150a0a9c094a66d768d254b59c12da7bd9735
Marcell <marcellnemeth02@gmail.com>
2023-04-16 18:10:55 +0200

    létrehozott cs fileok

---
+++ Services/ProjectService/ProjectService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RendszerRepo.Services.ProjectService
+{
+    public class ProjectService : IProjectService
+    {
+    
+    }
+}
